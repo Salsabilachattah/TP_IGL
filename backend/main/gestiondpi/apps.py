bafa2e94b3f143a78f237bdfdd3dcdfe82f8bb0d
Amar <ma_hassam@esi.dz>
2024-12-11 21:19:30 +0100

    change settings.py and models.py

--- backend/main/gestiondpi/apps.py
+++ backend/main/gestiondpi/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ConsultationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'consultation'
+    name = 'gestiondpi'
