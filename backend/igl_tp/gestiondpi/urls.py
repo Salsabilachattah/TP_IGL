a9fc372c85712596b99cc54ce8b455861d99fe4b
Amar <ma_hassam@esi.dz>
2024-12-12 14:16:57 +0100

    changed views folder

--- backend/igl_tp/gestiondpi/urls.py
+++ backend/igl_tp/gestiondpi/urls.py
@@ -1,6 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import CreateRolesGroupsView
+from .views.auth import CreateRolesGroupsView
 
 urlpatterns = [
     path('create-roles-groups/', CreateRolesGroupsView.as_view(), name='create_roles_groups'),
