d38fc53f6cc6cd339d0f1af146ac2ae958a9f406
Amar <ma_hassam@esi.dz>
2024-12-14 22:11:19 +0100

    add route and view of resume and diagnostic

--- backend/igl_tp/gestiondpi/urls.py
+++ backend/igl_tp/gestiondpi/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from .views.auth import CreateRolesGroupsView
 from .views.dpiView import PatientListCreateAPIView
-
+from views.consultation import ResumeView,DiagnostiqueView
 
 urlpatterns = [
     path('create-roles-groups/', CreateRolesGroupsView.as_view(), name='create_roles_groups'),
     path('patients/', PatientListCreateAPIView.as_view(), name='patients'),
     path('patients/<int:nss>/', PatientListCreateAPIView.as_view(), name='patient-detail'),
+    path('consultation/<int:id>/resume/', ResumeView.as_view(), name='consultation_resume'),
+    path('consultation/<int:id>/diagnostique/', DiagnostiqueView.as_view(), name='edit_diagnostique'),
 ]
---
+++ backend/igl_tp/gestiondpi/views/consultation.py
@@ -0,0 +1,43 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import Consultation
+from ..serializers.consultation import ResumeSerializer,DiagnostiqueSerializer
+from django.shortcuts import get_object_or_404
+
+class ResumeView(APIView):
+    def get(self, request, id):
+        # Get the consultation instance by id
+        consultation = get_object_or_404(Consultation, id=id)
+
+        # Return the resume field as part of the response
+        return Response({"resume": consultation.resume}, status=status.HTTP_200_OK)
+
+    def patch(self, request, id):
+        consultation = get_object_or_404(Consultation, id=id)
+        serializer = ResumeSerializer(consultation, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class DiagnostiqueView(APIView):
+    def get(self, request, id):
+        # Get the consultation instance by id
+        consultation = get_object_or_404(Consultation, id=id)
+
+        # Return the resume field as part of the response
+        return Response({"resume": consultation.diagnostique}, status=status.HTTP_200_OK)
+
+
+    def patch(self, request, id):
+        consultation = get_object_or_404(Consultation, id=id)
+        serializer = DiagnostiqueSerializer(consultation, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
