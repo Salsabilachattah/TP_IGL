a0539e1ffd2fa636a5f39523acfabfc2e4a5687a
Djinaneb <155325279+Djinaneb@users.noreply.github.com>
2025-01-04 19:59:08 +0100

    Update models.py

--- backend/igl_tp/gestiondpi/models.py
+++ backend/igl_tp/gestiondpi/models.py
@@ -103,6 +103,16 @@ class BilanBioTest(models.Model):
 
 
 
+class Medicament(models.Model):
+    nom = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nom
+
+
+
 
 class OrdonanceMedicament(models.Model):
     ordonance = models.ForeignKey(Ordonance, on_delete=models.CASCADE)
