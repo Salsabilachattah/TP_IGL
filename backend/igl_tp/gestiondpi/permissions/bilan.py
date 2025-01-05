2ed23d32593857f2c1973ff00ba10aec765dea9f
Khebabchi | خبابشي <127018810+khebabchi@users.noreply.github.com>
2025-01-05 20:56:25 +0100

    Update bilan.py

--- backend/igl_tp/gestiondpi/permissions/bilan.py
+++ backend/igl_tp/gestiondpi/permissions/bilan.py
@@ -1,7 +1,6 @@
-from rest_framework import permissions
+ from rest_framework import permissions
 
-
-class BilanBioPermissions(permissions.BasePermission):
+class BilanPermissions(permissions.BasePermission):
     """
     Custom permission to allow:
     - GET requests for users in the 'medecin', 'laboratorien', or 'patient' groups.
@@ -12,38 +11,14 @@ class BilanBioPermissions(permissions.BasePermission):
         # For GET method, allow access to 'medecin', 'laboratorien', or 'patient' groups
         if request.method == 'GET':
             # Check if the user belongs to any of the allowed groups
-            return request.user.groups.filter(name__in=['medecin', 'laboratorien', 'patient']).exists()
+            return request.user.groups.filter(name__in=['medecin', 'laboratorien', 'radiologue','patient']).exists()
 
         # For POST method, allow only 'medecin' group
-        elif request.method == 'POST':
-            return request.user.groups.filter(name='medecin').exists()
+        if request.method == 'POST':
+            return request.user.groups.filter(name__in='medecin').exists()
 
-        elif request.method in ['PATCH', 'OPTIONS']:
-            return request.user.groups.filter(name= 'laboratorien').exists()
-
-        # Default: deny any other methods (e.g., PUT, DELETE)
-        return False
-
-
-class BilanRadioPermissions(permissions.BasePermission):
-    """
-    Custom permission to allow:
-    - GET requests for users in the 'medecin', 'laboratorien', or 'patient' groups.
-    - POST requests only for users in the 'medecin' group.
-    """
-
-    def has_permission(self, request, view):
-        # For GET method, allow access to 'medecin', 'laboratorien', or 'patient' groups
-        if request.method == 'GET':
-            # Check if the user belongs to any of the allowed groups
-            return request.user.groups.filter(name__in=['medecin', 'radiologue', 'patient']).exists()
-
-        # For POST method, allow only 'medecin' group
-        elif request.method == 'POST':
-            return request.user.groups.filter(name='medecin').exists()
-
-        elif request.method in ['PATCH', 'OPTIONS']:
-            return request.user.groups.filter(name= 'radiologue').exists()
+        if request.method =='PATCH':
+            return request.user.groups.filter(name__in= [ 'laboratorien', 'radiologue']).exists()
 
         # Default: deny any other methods (e.g., PUT, DELETE)
         return False
