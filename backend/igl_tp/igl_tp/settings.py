b250015fa116f4e8759a46986f02d44c9e9416c6
Khebabchi | خبابشي <127018810+khebabchi@users.noreply.github.com>
2025-01-28 12:02:21 +0100

    Update settings.py

--- backend/igl_tp/igl_tp/settings.py
+++ backend/igl_tp/igl_tp/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'igl_tp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'igl-db_1',
+        'USER':'igl-db',
+        'PASSWORD':'Admin@123456',
+        'HOST':'mysql-igl-db.alwaysdata.net',
+        'PORT':'3306'
     }
 }
 
